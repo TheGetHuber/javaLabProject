ab0904b4f405af13bc9a50888dbd47bc919bf22c
TheGetHuber <reidolf.danila@gmail.com>
2025-11-16 13:29:25 +0300

    Particular Order service implemetation.

---
+++ orderservice/src/main/java/com/thegethuber/orderservice/services/OrderService.java
@@ -0,0 +1,16 @@
+package com.thegethuber.orderservice.services;
+
+import com.thegethuber.orderservice.dto.OrderItemRequestDto;
+import com.thegethuber.orderservice.dto.OrderItemResponseDto;
+import com.thegethuber.orderservice.dto.OrderRequestDto;
+import com.thegethuber.orderservice.dto.OrderResponseDto;
+
+import java.util.List;
+
+public interface OrderService {
+    OrderResponseDto placeOrder(OrderRequestDto orderRequestDto, List<OrderItemRequestDto> items);
+    OrderResponseDto updateOrder(Long id, String status);
+    OrderResponseDto getOrderById(Long id);
+    List<OrderResponseDto> getOrderByUserId(Integer id);
+    List<OrderResponseDto> getOrdersByRestaurantId(Integer restaurantId);
+}
